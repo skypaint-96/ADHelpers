e4159c8c5112f35552d32bc4b709f5e139e3441c
Mason Kerr <mason.mk@outlook.com>
2024-05-20 16:13:56 +0100

    Taken dependencies out

--- GetLockoutStatusCommand.cs
+++ GetLockoutStatusCommand.cs
@@ -4,7 +4,7 @@
     using System.Collections.Generic;
     using System.Management.Automation;
     using System.Management.Automation.Runspaces;
-    using System.DirectoryServices;
+    //using System.DirectoryServices;
 
 
 
@@ -22,9 +22,10 @@
         // This method will be called for each input received from the pipeline to this cmdlet; if no input is received, this method is not called
         protected override void ProcessRecord()
         {
-            DirectorySearcher searcher = new DirectorySearcher();
-            searcher.Filter = $"(name = \"{Identity}\"";
-            WriteObject(searcher.FindOne());
+            //DirectorySearcher searcher = new DirectorySearcher();
+            //searcher.Filter = $"(name = \"{Identity}\"";
+            //WriteObject(searcher.FindOne());
+            WriteObject($"hello {Identity}");
         }
 
     }
