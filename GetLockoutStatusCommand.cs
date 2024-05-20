a1c61ba277eaa4877ebbe85e9e6f68e89af9f9a7
Mason Kerr <mason.mk@outlook.com>
2024-05-20 16:32:31 +0100

    added directory services back

--- GetLockoutStatusCommand.cs
+++ GetLockoutStatusCommand.cs
@@ -4,7 +4,7 @@
     using System.Collections.Generic;
     using System.Management.Automation;
     using System.Management.Automation.Runspaces;
-    //using System.DirectoryServices;
+    using System.DirectoryServices;
 
 
 
@@ -22,10 +22,9 @@
         // This method will be called for each input received from the pipeline to this cmdlet; if no input is received, this method is not called
         protected override void ProcessRecord()
         {
-            //DirectorySearcher searcher = new DirectorySearcher();
-            //searcher.Filter = $"(name = \"{Identity}\"";
-            //WriteObject(searcher.FindOne());
-            WriteObject($"hello {Identity}");
+            DirectorySearcher searcher = new DirectorySearcher();
+            searcher.Filter = $"(name = \"{Identity}\"";
+            WriteObject(searcher.FindOne());
         }
 
     }
