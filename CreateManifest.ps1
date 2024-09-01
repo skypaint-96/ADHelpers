fa34ede76c8a697236b8444fb6df8e8469549def
Mason Kerr <mason.mk@outlook.com>
2024-09-02 00:27:30 +0100

    Updated CreateManifest.ps1 to be more reliable, and now is automaticly generated into each build

--- CreateManifest.ps1
+++ CreateManifest.ps1
@@ -1,26 +1,19 @@
-#$domain = [AppDomain]::CreateDomain("NewDomain");
+$f = {
+    import-module '.\adhelpers.dll' -scope local;
+    $version = (get-module AdHelpers).version;
+    remove-module adhelpers -force;
+    New-ModuleManifest -Path '.\AdHelpers.psd1' `
+        -RootModule 'ADHelpers' `
+        -moduleversion $version `
+        -Guid '55b99733-8488-493e-a3cd-05cfcea90974' `
+        -Author 'Mason Kerr' `
+        -CompanyName 'Pasifico' `
+        -Copyright '(c) 2024 Mason Kerr. All rights reserved.' `
+        -CmdletsToExport '*' `
+        -VariablesToExport '*' `
+        -LicenseUri 'https://github.com/skypaint-96/ADHelpers/blob/master/LICENSE.txt' `
+        -ProjectUri 'https://github.com/skypaint-96/ADHelpers' `
+        -DefaultCommandPrefix 'ADEX'
+}
 
-
-#$domain.DoCallBack({
-import-module ".\publish\adhelpers\adhelpers.dll" -scope local
-$version = (get-module AdHelpers).version
-remove-module adhelpers -force
-
-New-ModuleManifest -Path '.\publish\adhelpers\AdHelpers.psd1' `
--RootModule "ADHelpers" `
--moduleversion $version `
--Guid '55b99733-8488-493e-a3cd-05cfcea90974' `
--Author 'Mason Kerr' `
--CompanyName 'Pasifico' `
--Copyright '(c) 2024 Mason Kerr. All rights reserved.' `
--CmdletsToExport '*' `
--VariablesToExport '*' `
--LicenseUri 'https://github.com/skypaint-96/ADHelpers/blob/master/LICENSE.txt' `
--ProjectUri 'https://github.com/skypaint-96/ADHelpers' `
--DefaultCommandPrefix 'ADEX'
-
-exit
-#});
-
-
-#[AppDomain]::Unload($domain)
+start-process powershell -argumentlist ('-Command ' + $f.tostring())
